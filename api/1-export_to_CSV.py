4e6c8e494903a3cad6fc098df2e1879c226cede7
Excel Asaph <127494445+excelasaph@users.noreply.github.com>
2023-11-05 14:56:12 +0200

    Updating 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,4 +1,16 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""
+Using what you did in the task #0,
+extend your Python script to export
+data in the CSV format.
+
+Requirements:
+
+Records all tasks that are owned by this employee
+Format must be:
+"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
 if __name__ == "__main__":
     import csv
     import json
